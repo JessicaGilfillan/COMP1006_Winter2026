f0eca8a527e4e9695610652391f85ed905425645
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2026-03-20 14:08:58 -0400

    finished code

--- 10/includes/footer.php
+++ 10/includes/footer.php
@@ -1,3 +1,7 @@
 <footer>
-    <p> Bake It Til You Make It - COMP1006 </p>
-  </footer>
+  <p> Bake It Til You Make It - COMP1006 </p>
+</footer>
+<script src="https://cdn.jsdelivr.net" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+</html>
